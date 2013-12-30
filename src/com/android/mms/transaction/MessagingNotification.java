95079288cb029d0564bed74291683b79d865375e
DvTonder <david.vantonder@gmail.com>
2013-12-30 09:32:17 -0500

    Mms: Port custom notification vibrations
    Change-Id: I24ec97e3a33aaf0cbfd025b7b29dd0e0fb22343a

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -992,7 +992,14 @@ public class MessagingNotification {
                 vibrate = "always".equals(vibrateWhen);
             }
             if (vibrate) {
-                defaults |= Notification.DEFAULT_VIBRATE;
+                String pattern = sp.getString(
+                        MessagingPreferenceActivity.NOTIFICATION_VIBRATE_PATTERN, "0,1200");
+
+                if (!TextUtils.isEmpty(pattern)) {
+                    noti.setVibrate(parseVibratePattern(pattern));
+                } else {
+                    defaults |= Notification.DEFAULT_VIBRATE;
+                }
             }
 
             String ringtoneStr = sp.getString(MessagingPreferenceActivity.NOTIFICATION_RINGTONE,
@@ -1521,5 +1528,21 @@ public class MessagingNotification {
         } finally {
             cursor.close();
         }
+    }
+
+    // Parse the user provided custom vibrate pattern into a long[]
+    private static long[] parseVibratePattern(String pattern) {
+        String[] splitPattern = pattern.split(",");
+        long[] result = new long[splitPattern.length];
+
+        for (int i = 0; i < splitPattern.length; i++) {
+            try {
+                result[i] = Long.parseLong(splitPattern[i]);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+        return result;
     }
 }
