e96fcef7bca592c43fb2482e16c1f14c3c429c62
Zhang Fang <zfang@codeaurora.org>
2014-04-18 18:24:36 +0800

    MMS: Refresh SIM messages if SIM card state changed.
    SIM messages still display when airplane mode is on.
    In Manage SIM card messages view, receive
     TelephonyIntents.ACTION_SIM_STATE_CHANGED to refresh SIM messages.
    
    CRs-Fixed: 651995
    Change-Id: I93f3925f51dd660cc15f235bfbdf0febf86cdb2b

--- src/com/android/mms/ui/ManageSimMessages.java
+++ src/com/android/mms/ui/ManageSimMessages.java
@@ -21,10 +21,13 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.AsyncQueryHandler;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
@@ -51,6 +54,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.internal.telephony.MSimConstants;
+import com.android.internal.telephony.TelephonyIntents;
 import com.android.mms.data.Contact;
 import com.android.mms.R;
 import com.android.mms.transaction.MessagingNotification;
@@ -101,6 +105,16 @@ public class ManageSimMessages extends Activity
         }
     };
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (TelephonyIntents.ACTION_SIM_STATE_CHANGED.equals(action)) {
+                refreshMessageList();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -118,6 +132,10 @@ public class ManageSimMessages extends Activity
 
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TelephonyIntents.ACTION_SIM_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
 
         init();
     }
