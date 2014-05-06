7c993f58da08f4ffaac91b1fc28ea8f11c59df42
yanglv <yanglv@codeaurora.org>
2014-05-06 17:08:46 +0800

    Mms: No delivery check-mark if both delivery and read report enabled
    If read report enabled, it always displays "detail" icon on UI, no
    matter what if delivery report or read report enabled.
    
    Change the check-mark avaiable only for delivery report.
    If only "read report" enabled, just show "detail" icon instead.
    
    Change-Id: I68306ede310fddc949cd8d2c1df865f634aeae62
    CRs-Fixed: 648733

--- src/com/android/mms/ui/MessageListItem.java
+++ src/com/android/mms/ui/MessageListItem.java
@@ -1017,7 +1017,7 @@ public class MessageListItem extends LinearLayout implements
         // we show the icon if the read report or delivery report setting was set when the
         // message was sent. Showing the icon tells the user there's more information
         // by selecting the "View report" menu.
-        if (msgItem.mDeliveryStatus == MessageItem.DeliveryStatus.INFO || msgItem.mReadReport
+        if (msgItem.mDeliveryStatus == MessageItem.DeliveryStatus.INFO
                 || (msgItem.isMms() && !msgItem.isSending() &&
                         msgItem.mDeliveryStatus == MessageItem.DeliveryStatus.PENDING)) {
             mDetailsIndicator.setImageResource(R.drawable.ic_sms_mms_details);
@@ -1025,6 +1025,9 @@ public class MessageListItem extends LinearLayout implements
         } else if (msgItem.isMms() && !msgItem.isSending() &&
                 msgItem.mDeliveryStatus == MessageItem.DeliveryStatus.RECEIVED) {
             mDetailsIndicator.setImageResource(R.drawable.ic_sms_mms_delivered);
+            mDetailsIndicator.setVisibility(View.VISIBLE);
+        } else if (msgItem.mReadReport) {
+            mDetailsIndicator.setImageResource(R.drawable.ic_sms_mms_details);
             mDetailsIndicator.setVisibility(View.VISIBLE);
         } else {
             mDetailsIndicator.setVisibility(View.GONE);
