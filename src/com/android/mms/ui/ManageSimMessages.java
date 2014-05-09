miscdec/caf-Mms

Mms: Show statistics of SIM card message.
Add sim capacity function on SIM card message view.

Change-Id: If2967e0cd40753e0fe78e1f77eaa8ef76822ed8b
CRs-Fixed: 645022

src/com/android/mms/ui/ManageSimMessages.java
@@ -38,6 +38,8 @@ import android.os.Handler;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.Telephony.Sms;
+import android.telephony.MSimSmsManager;
+import android.telephony.MSimTelephonyManager;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.text.SpannableString;
@@ -78,6 +80,9 @@ public class ManageSimMessages extends Activity
     private static final int MENU_ADD_ADDRESS_TO_CONTACTS = 6;
     private static final int MENU_SEND_EMAIL = 7;
     private static final int OPTION_MENU_DELETE_ALL = 0;
+    private static final int OPTION_MENU_SIM_CAPACITY = 1;
+
+    private static final int SUB_INVALID = -1;
 
     private static final int SHOW_LIST = 0;
     private static final int SHOW_EMPTY = 1;
@@ -497,6 +502,7 @@ public class ManageSimMessages extends Activity
         if (mState == SHOW_LIST && (null != mCursor) && (mCursor.getCount() > 0)) {
             menu.add(0, OPTION_MENU_DELETE_ALL, 0, R.string.menu_delete_messages).setIcon(
                     android.R.drawable.ic_menu_delete);
+            menu.add(0, OPTION_MENU_SIM_CAPACITY, 0, R.string.sim_capacity_title);
         }
 
         return true;
@@ -528,6 +534,10 @@ public class ManageSimMessages extends Activity
                 };
                 break;
 
+            case OPTION_MENU_SIM_CAPACITY:
+                showSimCapacityDialog();
+                break;
+
             case android.R.id.home:
                 // The user clicked on the Messaging icon in the action bar. Take them back from
                 // wherever they came from
@@ -546,6 +556,28 @@ public class ManageSimMessages extends Activity
         builder.setPositiveButton(R.string.yes, listener);
         builder.setNegativeButton(R.string.no, null);
         builder.setMessage(messageId);
+
+        builder.show();
+    }
+
+    private void showSimCapacityDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.sim_capacity_title);
+        builder.setCancelable(true);
+        builder.setPositiveButton(R.string.yes, null);
+        StringBuilder capacityMessage = new StringBuilder();
+        capacityMessage.append(getString(R.string.sim_capacity_used));
+        capacityMessage.append(" " + mCursor.getCount() + "\n");
+        capacityMessage.append(getString(R.string.sim_capacity_all));
+        int iccCapacityAll = -1;
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+            iccCapacityAll = MSimSmsManager.getDefault().getSmsCapacityOnIcc(mSubscription);
+        } else {
+            iccCapacityAll = SmsManager.getDefault().getSmsCapacityOnIcc();
+        }
+
+        capacityMessage.append(" " + iccCapacityAll);
+        builder.setMessage(capacityMessage);
 
         builder.show();
     }

CyanogenMod/android_packages_apps_Mms-caf

src/com/android/mms/ui/ManageSimMessages.java
@@ -35,6 +35,8 @@ import android.os.Handler;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.provider.Telephony.Sms;
+import android.telephony.MSimSmsManager;
+import android.telephony.MSimTelephonyManager;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.text.SpannableString;
@@ -74,6 +76,9 @@ public class ManageSimMessages extends Activity
     private static final int MENU_ADD_ADDRESS_TO_CONTACTS = 6;
     private static final int MENU_SEND_EMAIL = 7;
     private static final int OPTION_MENU_DELETE_ALL = 0;
+    private static final int OPTION_MENU_SIM_CAPACITY = 1;
+
+    private static final int SUB_INVALID = -1;
 
     private static final int SHOW_LIST = 0;
     private static final int SHOW_EMPTY = 1;
@@ -476,6 +481,7 @@ public class ManageSimMessages extends Activity
         if (mState == SHOW_LIST && (null != mCursor) && (mCursor.getCount() > 0)) {
             menu.add(0, OPTION_MENU_DELETE_ALL, 0, R.string.menu_delete_messages).setIcon(
                     android.R.drawable.ic_menu_delete);
+            menu.add(0, OPTION_MENU_SIM_CAPACITY, 0, R.string.sim_capacity_title);
         }
 
         return true;
@@ -507,6 +513,10 @@ public class ManageSimMessages extends Activity
                 };
                 break;
 
+            case OPTION_MENU_SIM_CAPACITY:
+                showSimCapacityDialog();
+                break;
+
             case android.R.id.home:
                 // The user clicked on the Messaging icon in the action bar. Take them back from
                 // wherever they came from
@@ -525,6 +535,28 @@ public class ManageSimMessages extends Activity
         builder.setPositiveButton(R.string.yes, listener);
         builder.setNegativeButton(R.string.no, null);
         builder.setMessage(messageId);
+
+        builder.show();
+    }
+
+    private void showSimCapacityDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.sim_capacity_title);
+        builder.setCancelable(true);
+        builder.setPositiveButton(R.string.yes, null);
+        StringBuilder capacityMessage = new StringBuilder();
+        capacityMessage.append(getString(R.string.sim_capacity_used));
+        capacityMessage.append(" " + mCursor.getCount() + "\n");
+        capacityMessage.append(getString(R.string.sim_capacity));
+        int iccCapacityAll = -1;
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+            iccCapacityAll = MSimSmsManager.getDefault().getSmsCapacityOnIcc(mSubscription);
+        } else {
+            iccCapacityAll = SmsManager.getDefault().getSmsCapacityOnIcc();
+        }
+
+        capacityMessage.append(" " + iccCapacityAll);
+        builder.setMessage(capacityMessage);
 
         builder.show();
     }