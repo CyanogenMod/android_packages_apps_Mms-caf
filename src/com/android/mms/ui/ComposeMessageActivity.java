a479ce22dddb8505d6f67ee6df06b3a3196e7868
Vladislav Koldobskiy <admin@nevergone.ru>
2014-06-08 17:32:40 +0400

    Hide "Insert smiley" if emoticons are disabled
    There's no point in having "insert smiley" option when
    graphical emoticons are disabled.
    I still hate these emoticons.
    
    Change-Id: I2b811015910811e211c9e3e203532f19c29a0638

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -414,6 +414,9 @@ public class ComposeMessageActivity extends Activity
     private AsyncDialog mAsyncDialog;   // Used for background tasks.
 
     private String mDebugRecipients;
+
+    private boolean mEnableEmoticons;
+
     private int mLastSmoothScrollPosition;
     private boolean mScrollOnSend;      // Flag that we need to scroll the list to the end.
 
@@ -2017,6 +2020,8 @@ public class ComposeMessageActivity extends Activity
             mUnicodeFilter = new UnicodeFilter(stripNonDecodableOnly);
         }
 
+        mEnableEmoticons = prefs.getBoolean(MessagingPreferenceActivity.ENABLE_EMOTICONS, true);
+
         View inflate = getLayoutInflater().inflate(R.layout.compose_message_activity, null);
         mZoomGestureOverlayView = new ZoomGestureOverlayView(this);
         mZoomGestureOverlayView.addZoomListener(this);
@@ -3019,7 +3024,7 @@ public class ComposeMessageActivity extends Activity
             menu.add(0, MENU_IMPORT_TEMPLATE, 0, R.string.import_message_template);
         }
 
-        if (!mWorkingMessage.hasSlideshow() && mIsSmsEnabled) {
+        if (!mWorkingMessage.hasSlideshow() && mIsSmsEnabled && mEnableEmoticons) {
             menu.add(0, MENU_INSERT_SMILEY, 0, R.string.menu_insert_smiley).setIcon(
                     R.drawable.ic_menu_emoticons);
         }
