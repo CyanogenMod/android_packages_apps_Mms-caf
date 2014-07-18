a417d17cc4faaf7eb76ea09bf7070949cbca03c2
Jorge Ruesga <jorge@ruesga.com>
2014-07-18 23:34:48 +0200

    mms: markAllAsUnread is never called
    The actual markAllAsUnread code is never called. It requires a new menu to be launched.
    
    Change-Id: If620ca3c41aa091e065ff7646091bc118a072e5d

--- src/com/android/mms/ui/ConversationList.java
+++ src/com/android/mms/ui/ConversationList.java
@@ -687,6 +687,11 @@ public class ConversationList extends ListActivity implements DraftCache.OnDraft
             }
         }
 
+        item = menu.findItem(R.id.action_mark_all_as_unread);
+        if (item != null) {
+            item.setVisible((mListAdapter.getCount() > 0) && mIsSmsEnabled);
+        }
+
         if (!LogTag.DEBUG_DUMP) {
             item = menu.findItem(R.id.action_debug_dump);
             if (item != null) {
@@ -722,6 +727,11 @@ public class ConversationList extends ListActivity implements DraftCache.OnDraft
             case R.id.action_delete_all:
                 // The invalid threadId of -1 means all threads here.
                 confirmDeleteThread(-1L, mQueryHandler);
+                break;
+            case R.id.action_mark_all_as_unread:
+                final MarkAsUnreadThreadListener listener = new MarkAsUnreadThreadListener(
+                        null, mQueryHandler, this);
+                confirmMarkAsUnreadDialog(listener, null, this);
                 break;
             case R.id.action_settings:
                 Intent intent = new Intent(this, MessagingPreferenceActivity.class);
