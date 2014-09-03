3079ec08d1342456688df69822026fec34de13ea
Jorge Ruesga <jorge@ruesga.com>
2014-09-04 01:06:39 +0200

    mms: do not close database
    JIRA: BUGDUMP-22916
    https://jira.cyanogenmod.org/browse/BUGDUMP-22916
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Change-Id: I1ce4d8e3d8b1bb482e5556e20e9bb76dbf522abf

--- src/com/android/mms/data/cm/CMMmsDatabaseHelper.java
+++ src/com/android/mms/data/cm/CMMmsDatabaseHelper.java
@@ -67,7 +67,6 @@ public class CMMmsDatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(CONVERSATIONS_VIBRATE_ENABLED, conversationSettings.mVibrateEnabled);
         contentValues.put(CONVERSATIONS_VIBRATE_PATTERN, conversationSettings.mVibratePattern);
         db.insert(CONVERSATIONS_TABLE, null, contentValues);
-        db.close();
     }
 
     // boolean version
@@ -78,7 +77,6 @@ public class CMMmsDatabaseHelper extends SQLiteOpenHelper {
         db.update(CONVERSATIONS_TABLE, contentValues,
                 " " + CONVERSATIONS_THREAD_ID + " = ?",
                 new String[]{String.valueOf(threadId)});
-        db.close();
     }
 
     // string version
@@ -89,7 +87,6 @@ public class CMMmsDatabaseHelper extends SQLiteOpenHelper {
         db.update(CONVERSATIONS_TABLE, contentValues,
                 " " + CONVERSATIONS_THREAD_ID + " = ?",
                 new String[]{String.valueOf(threadId)});
-        db.close();
     }
 
     public void updateCMConversationSettings(CMConversationSettings conversationSettings) {
