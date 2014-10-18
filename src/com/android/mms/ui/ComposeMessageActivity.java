d84e4ff626a4a1b7e0486ca33a368d3c3b5ce53c
kaiyiz <kaiyiz@codeaurora.org>
2014-10-18 15:25:30 +0800

    Mms: Fix Send button is gray when insert ring tone and then input number.
    Because remove recipient change listener on activity pause, but add
    it on activity start that don't be called after add ring tone.
    
    Change adding recipient change listener to activity resume.
    
    CRs-Fixed: 740954
    
    Change-Id: I712836044ba22263d4a77c4d498e053fa6dab18a

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2511,6 +2511,9 @@ public class ComposeMessageActivity extends Activity
 
         mIsPickingContact = false;
         addRecipientsListeners();
+        if (isRecipientsEditorVisible()) {
+            mRecipientsEditor.addTextChangedListener(mRecipientsWatcher);
+        }
 
         if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
             log("update title, mConversation=" + mConversation.toString());
