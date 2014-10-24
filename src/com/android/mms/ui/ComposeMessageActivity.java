9a041464c25b018a005962560b246a9b809289e3
kaiyiz <kaiyiz@codeaurora.org>
2014-10-24 17:42:42 +0800

    Mms: Fix SMS/MMS draft's phone number can't be changed.
    Because initialize recipient editor after activity resume when edit
    draft, so don't add recipient changed listener in activity resume.
    
    Add recipient changed listener after query complete for editing draft.
    
    CRs-Fixed: 741024
    
    Change-Id: I630adc26878fdd16f66cd170f289c65766c47255

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -5068,6 +5068,7 @@ public class ComposeMessageActivity extends Activity
                     if (cursor != null && cursor.getCount() == 0
                             && !isRecipientsEditorVisible() && !mSentMessage) {
                         initRecipientsEditor();
+                        mRecipientsEditor.addTextChangedListener(mRecipientsWatcher);
                     }
 
                     // FIXME: freshing layout changes the focused view to an unexpected
