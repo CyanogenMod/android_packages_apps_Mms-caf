625a1f81ccf89ba738a7ec638e21b091c849ece3
kaiyiz <kaiyiz@codeaurora.org>
2014-11-17 09:28:45 +0800

    Mms: Fix MMS notification isn't disappeared.
    From notification launched message detail, mms didn't
    update the 'seen' field in database for message,
    It caused message's notification cann't disappear.
    
    Add update 'seen' field from 0 to 1 operation
    in message detail's onCreate().
    
    CRs-Fixed: 755385
    
    Change-Id: I2c0554cba9ef3dcaefc7ce72fcdb0e67d99c4796

--- src/com/android/mms/ui/MobilePaperShowActivity.java
+++ src/com/android/mms/ui/MobilePaperShowActivity.java
@@ -85,8 +85,6 @@ public class MobilePaperShowActivity extends Activity {
     private static final int MENU_REPLY = 3;
     private static final int MENU_FORWARD = 4;
 
-    private static final int MESSAGE_READ = 1;
-
     private int mMailboxId = -1;
 
     private FrameLayout mSlideView;
@@ -207,8 +205,9 @@ public class MobilePaperShowActivity extends Activity {
     private void markAsReadIfNeed() {
         boolean unread = mIntent.getBooleanExtra("unread", false);
         if (unread) {
-            ContentValues values = new ContentValues(1);
-            values.put(Mms.READ, MESSAGE_READ);
+            ContentValues values = new ContentValues(2);
+            values.put(Mms.SEEN, MessageUtils.MESSAGE_SEEN);
+            values.put(Mms.READ, MessageUtils.MESSAGE_READ);
             SqliteWrapper.update(this, getContentResolver(),
                     mUri, values, null, null);
 
