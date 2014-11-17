e960729e517fbfcd62708ad38a2e0685455a9ef9
kaiyiz <kaiyiz@codeaurora.org>
2014-11-17 09:58:42 +0800

    Mms:Fix can't pop up slot selection when send a new SMS
    If the recipients editor is visible,will not show slot selection
    and send message directly.
    
    When the recipients editor is visible and multi sim is enable
    add sendMsimMessage() to show slot selection.
    
    CRs-Fixed: 752417
    
    Change-Id: Ia9422f41a2962f61ba4e58ad8b0594e5d7e4eca6

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -1055,7 +1055,11 @@ public class ComposeMessageActivity extends Activity
             // as the destination.
             ContactList contacts = mRecipientsEditor.constructContactsFromInput(false);
             mDebugRecipients = contacts.serialize();
-            sendMessage(true);
+            if ((TelephonyManager.getDefault().getPhoneCount()) > 1) {
+                sendMsimMessage(true);
+            } else {
+                sendMessage(true);
+            }
         }
     }
 
