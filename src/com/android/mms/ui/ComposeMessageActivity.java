96191c9004e5360d3b2f8cc2856e81ff794b5b36
kaiyiz <kaiyiz@codeaurora.org>
2014-11-18 14:20:20 +0800

    MMS: Fix display session mode after send message
    It won't exit send will query message list in ComposeMessageActivity
    after send a message in folder mode.
    
    Do not query and show message list in folder mode.
    Exit ComposeMessageActivity after send a message in folder mode.
    
    CRs-Fixed: 659985
    
    Change-Id: Iae51c25ad68c9c59815620d9538e13c982df2406

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4378,7 +4378,7 @@ public class ComposeMessageActivity extends Activity
     }
 
     private void startMsgListQuery(int token) {
-        if (mSendDiscreetMode) {
+        if (mSendDiscreetMode || MessageUtils.isMailboxMode()) {
             return;
         }
         Uri conversationUri = mConversation.getUri();
@@ -4425,7 +4425,8 @@ public class ComposeMessageActivity extends Activity
         mMsgListAdapter.setMsgListItemHandler(mMessageListItemHandler);
         mMsgListView.setAdapter(mMsgListAdapter);
         mMsgListView.setItemsCanFocus(false);
-        mMsgListView.setVisibility(mSendDiscreetMode ? View.INVISIBLE : View.VISIBLE);
+        mMsgListView.setVisibility((mSendDiscreetMode || MessageUtils.isMailboxMode())
+                ? View.INVISIBLE : View.VISIBLE);
         mMsgListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -4627,7 +4628,7 @@ public class ComposeMessageActivity extends Activity
             mScrollOnSend = true;   // in the next onQueryComplete, scroll the list to the end.
         }
         // But bail out if we are supposed to exit after the message is sent.
-        if (mSendDiscreetMode) {
+        if (mSendDiscreetMode || MessageUtils.isMailboxMode()) {
             finish();
         }
     }
