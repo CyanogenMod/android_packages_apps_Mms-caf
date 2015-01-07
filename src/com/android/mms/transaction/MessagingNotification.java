b78335a5bccd8167505785ec90ff0a2fb5673752
Vladislav Koldobskiy <admin@nevergone.ru>
2015-01-07 20:11:11 +0300

    Mms: improve strings
    Forward-port from cm-11.0 + more improvements
    Restored zh-rCN/strings.xml and zh-rTW/strings.xml from AOSP
    to avoid future conflicts.
    Briefly touched SMS backup feature to have better wording, too.
    Removed more strings, unified 3-4 string references into one.
    
    Change-Id: Iaf7b2306ed9967507626e723e3c2cfd53e3435e3

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -1258,7 +1258,7 @@ public class MessagingNotification {
                 qmIntent.putExtra(QuickMessagePopup.QR_SHOW_KEYBOARD_EXTRA, true);
 
                 // Create the pending intent and add it to the notification
-                CharSequence qmText = context.getText(R.string.qm_quick_reply);
+                CharSequence qmText = context.getText(R.string.menu_reply);
                 PendingIntent qmPendingIntent = PendingIntent.getActivity(context, 0, qmIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
                 noti.addAction(R.drawable.ic_reply, qmText, qmPendingIntent);
@@ -1974,7 +1974,7 @@ public class MessagingNotification {
                 Context.NOTIFICATION_SERVICE);
 
         String title = context.getString(R.string.sms_full_title);
-        String description = context.getString(R.string.sms_full_body_cm);
+        String description = context.getString(R.string.sms_full_body);
         PendingIntent intent = PendingIntent.getActivity(context, 0,  new Intent(), 0);
         Notification notification = new Notification();
         notification.icon = R.drawable.stat_notify_sms_failed;
