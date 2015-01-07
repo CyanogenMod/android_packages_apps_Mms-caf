e3447c00e380c576467a5e8d2b647b44d74beba9
Vladislav Koldobskiy <admin@nevergone.ru>
2015-01-07 20:11:11 +0300

    Mms: improve strings
    Forward-port from cm-11.0 + more improvements
    Restored zh-rCN/strings.xml and zh-rTW/strings.xml from AOSP
    to avoid future conflicts.
    Briefly touched SMS backup feature to have better wording, too.
    Removed more strings, unified 3-4 string references into one.
    
    Change-Id: Iaf7b2306ed9967507626e723e3c2cfd53e3435e3

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3217,7 +3217,7 @@ public class ComposeMessageActivity extends Activity
 
         TemplateSelectListener listener = new TemplateSelectListener(smsTempArray);
         return new AlertDialog.Builder(ComposeMessageActivity.this)
-                .setTitle(R.string.message_template)
+                .setTitle(R.string.import_message_template)
                 .setItems(smsTempArray, listener)
                 .create();
     }
@@ -4232,8 +4232,9 @@ public class ComposeMessageActivity extends Activity
                 updateSendButtonState();
                 if (s.toString().getBytes().length == SUBJECT_MAX_LENGTH
                         && before < SUBJECT_MAX_LENGTH) {
-                    Toast.makeText(ComposeMessageActivity.this,
-                            R.string.subject_full, Toast.LENGTH_SHORT).show();
+                    String toast = getString(R.string.subject_full, SUBJECT_MAX_LENGTH);
+                    Toast.makeText(ComposeMessageActivity.this, toast,
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         }
@@ -4242,13 +4243,11 @@ public class ComposeMessageActivity extends Activity
         public void afterTextChanged(Editable s) {
             if (s.toString().getBytes().length > SUBJECT_MAX_LENGTH) {
                 String subject = s.toString();
-                Toast.makeText(ComposeMessageActivity.this,
-                        R.string.subject_full, Toast.LENGTH_SHORT).show();
-                while (subject.getBytes().length > SUBJECT_MAX_LENGTH) {
-                    subject = subject.substring(0, subject.length() - 1);
-                }
+                String toast = getString(R.string.subject_full, SUBJECT_MAX_LENGTH);
+                Toast.makeText(ComposeMessageActivity.this, toast,
+                        Toast.LENGTH_SHORT).show();
                 s.clear();
-                s.append(subject);
+                s.append(subject.substring(0, SUBJECT_MAX_LENGTH));
             }
         }
     };
@@ -5295,7 +5294,7 @@ public class ComposeMessageActivity extends Activity
             default:
                 break;
             }
-            String toast = getString(R.string.copy_to_sim_success, sum, success);
+            String toast = getString(R.string.copy_to_sim_success, success, sum);
             Toast.makeText(ComposeMessageActivity.this, toast,
                     Toast.LENGTH_SHORT).show();
         }
