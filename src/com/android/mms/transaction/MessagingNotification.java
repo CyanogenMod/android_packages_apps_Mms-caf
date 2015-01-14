AICP/packages_apps_Mms

QuickMessage: bring back mark as read
Squash of:
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/54462d03f211294e2831cecc46291ed63f94c858
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/d610689b8d69309aeadd82c64fec86f32fd150b0
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/77b6ea1ab78632557ddc227eff8b12c32b829c8a
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/8af6a0fa9c092b3b9c2f3a984216fe44ad160de4
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/adf6c597d0f562cf0e6ee83216b1ab76b9c8824c
https://github.com/CyanogenMod/android_packages_apps_Mms/commit/7afe23ff0f953e46891253b15fc3918b80195c28

Slight change in MessagingNotification.java under L115
private -> public for NOTIFICATION_ID = 123

Icon probably needs material redesign and other resolutions, but wanted to
push this so you guys can finish it up as CM likes to do.

What I've tested, it works just fine

Also setting author as DvTonder

Change-Id: Ic7d4de9c337436b288e3b570da5f1958cd14abb4

src/com/android/mms/transaction/MessagingNotification.java
@@ -81,6 +81,7 @@ import com.android.mms.data.Conversation;
 import com.android.mms.data.WorkingMessage;
 import com.android.mms.model.SlideModel;
 import com.android.mms.model.SlideshowModel;
+import com.android.mms.quickmessage.QmMarkRead;
 import com.android.mms.quickmessage.QuickMessagePopup;
 import com.android.mms.ui.ComposeMessageActivity;
 import com.android.mms.ui.ConversationList;
@@ -111,7 +112,7 @@ public class MessagingNotification {
     private static final String TAG = LogTag.APP;
     private static final boolean DEBUG = false;
 
-    private static final int NOTIFICATION_ID = 123;
+    public static final int NOTIFICATION_ID = 123;
     public static final int FULL_NOTIFICATION_ID   = 125;
     private static final int ICC_NOTIFICATION_ID_SLOT1 = 126;
     private static final int ICC_NOTIFICATION_ID_SLOT2 = 127;
@@ -1257,26 +1258,36 @@ public class MessagingNotification {
         final Notification notification;
 
         if (messageCount == 1 || uniqueThreadCount == 1) {
-            // Add the QuickMessage action only if the pop-up won't be shown already
+            // Add the Quick Reply action only if the pop-up won't be shown already
             if (!qmPopupEnabled && qmIntent != null) {
 
                 // This is a QR, we should show the keyboard when the user taps to reply
                 qmIntent.putExtra(QuickMessagePopup.QR_SHOW_KEYBOARD_EXTRA, true);
 
-                // Create the pending intent and add it to the notification
+                // Create the Quick reply pending intent and add it to the notification
                 CharSequence qmText = context.getText(R.string.qm_quick_reply);
                 PendingIntent qmPendingIntent = PendingIntent.getActivity(context, 0, qmIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
                 noti.addAction(R.drawable.ic_reply, qmText, qmPendingIntent);
             }
 
+            // Add the 'Mark as read' action
+            CharSequence markReadText = context.getText(R.string.qm_mark_read);
+            Intent mrIntent = new Intent();
+            mrIntent.setClass(context, QmMarkRead.class);
+            mrIntent.putExtra(QmMarkRead.SMS_THREAD_ID, mostRecentNotification.mThreadId);
+            PendingIntent mrPendingIntent = PendingIntent.getBroadcast(context, 0, mrIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            noti.addAction(R.drawable.ic_menu_read, markReadText, mrPendingIntent);
+
             // Add the Call action
             CharSequence callText = context.getText(R.string.menu_call);
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(mostRecentNotification.mSender.getPhoneUri());
-            PendingIntent mCallPendingIntent = PendingIntent.getActivity(context, 0, callIntent,
+            PendingIntent callPendingIntent = PendingIntent.getActivity(context, 0, callIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT);
-            noti.addAction(R.drawable.ic_menu_call, callText, mCallPendingIntent);
+            noti.addAction(R.drawable.ic_menu_call, callText, callPendingIntent);
+
         }
 
         if (messageCount == 1) {

CyanogenMod/android_packages_apps_Mms-caf

src/com/android/mms/transaction/MessagingNotification.java
@@ -84,6 +84,7 @@ import com.android.mms.data.Conversation;
 import com.android.mms.data.WorkingMessage;
 import com.android.mms.model.SlideModel;
 import com.android.mms.model.SlideshowModel;
+import com.android.mms.quickmessage.QmMarkRead;
 import com.android.mms.quickmessage.QuickMessagePopup;
 import com.android.mms.quickmessage.QuickMessageWear;
 import com.android.mms.ui.ComposeMessageActivity;
@@ -115,7 +116,7 @@ public class MessagingNotification {
     private static final String TAG = LogTag.APP;
     private static final boolean DEBUG = false;
 
-    private static final int NOTIFICATION_ID = 123;
+    public static final int NOTIFICATION_ID = 123;
     public static final int FULL_NOTIFICATION_ID   = 125;
     private static final int ICC_NOTIFICATION_ID_SLOT1 = 126;
     private static final int ICC_NOTIFICATION_ID_SLOT2 = 127;
@@ -1251,13 +1252,13 @@ public class MessagingNotification {
         WearableExtender wearableExtender = new WearableExtender();
 
         if (messageCount == 1 || uniqueThreadCount == 1) {
-            // Add the QuickMessage action only if the pop-up won't be shown already
+            // Add the Quick Reply action only if the pop-up won't be shown already
             if (!qmPopupEnabled && qmIntent != null) {
 
                 // This is a QR, we should show the keyboard when the user taps to reply
                 qmIntent.putExtra(QuickMessagePopup.QR_SHOW_KEYBOARD_EXTRA, true);
 
-                // Create the pending intent and add it to the notification
+                // Create the Quick reply pending intent and add it to the notification
                 CharSequence qmText = context.getText(R.string.qm_quick_reply);
                 PendingIntent qmPendingIntent = PendingIntent.getActivity(context, 0, qmIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
@@ -1268,17 +1269,26 @@ public class MessagingNotification {
                     R.drawable.ic_reply, qmText, qmPendingIntent).build()));
             }
 
+            // Add the 'Mark as read' action
+            CharSequence markReadText = context.getText(R.string.qm_mark_read);
+            Intent mrIntent = new Intent();
+            mrIntent.setClass(context, QmMarkRead.class);
+            mrIntent.putExtra(QmMarkRead.SMS_THREAD_ID, mostRecentNotification.mThreadId);
+            PendingIntent mrPendingIntent = PendingIntent.getBroadcast(context, 0, mrIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            noti.addAction(R.drawable.ic_mark_read, markReadText, mrPendingIntent);
+
             // Add the Call action
             CharSequence callText = context.getText(R.string.menu_call);
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(mostRecentNotification.mSender.getPhoneUri());
-            PendingIntent mCallPendingIntent = PendingIntent.getActivity(context, 0, callIntent,
+            PendingIntent callPendingIntent = PendingIntent.getActivity(context, 0, callIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT);
-            noti.addAction(R.drawable.ic_menu_call, callText, mCallPendingIntent);
+            noti.addAction(R.drawable.ic_menu_call, callText, callPendingIntent);
 
             //Wearable
             noti.extend(wearableExtender.addAction( new NotificationCompat.Action.Builder(
-                R.drawable.ic_menu_call, callText, mCallPendingIntent).build()));
+                R.drawable.ic_menu_call, callText, callPendingIntent).build()));
 
             //Set up remote input
             String replyLabel = context.getString(R.string.qm_wear_voice_reply);