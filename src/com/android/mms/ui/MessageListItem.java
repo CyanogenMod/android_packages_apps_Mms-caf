b7f0ba5fca0e1a27b40ceb9d3e1705a49d85bcf5
Matt Garnes <matt@cyngn.com>
2015-01-23 13:32:52 +0800

    Apply zoom scale to all items in ListView.
    - The zoom scale only affected items in the list that were currently
      visible. If you scroll, it does not stay applied.
    - Implement the adapter such that it will apply the zoom scale to every
      item as you scroll.
    - Store the zoom level in each item and format the text that will be
      displayed, such that as soon as the TextView enters the visible
      portion of the ListView, it will be rendered at the right size.
    
    Change-Id: I116f3bf5f6bdb1c2b4cd6e4df5c5ef78fc222a96

--- src/com/android/mms/ui/MessageListItem.java
+++ src/com/android/mms/ui/MessageListItem.java
@@ -57,6 +57,7 @@ import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.LineHeightSpan;
 import android.text.style.StyleSpan;
@@ -64,6 +65,7 @@ import android.text.style.TextAppearanceSpan;
 import android.text.style.URLSpan;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -279,6 +281,7 @@ public class MessageListItem extends ZoomMessageListItem implements
                                             mMessageItem.mHighlight,
                                             mMessageItem.mTextContentType));
 
+        mBodyTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mZoomFontSize);
         mDateView.setText(buildTimestampLine(msgSizeText + " " + mMessageItem.mTimestamp));
 
         updateSimIndicatorView(mMessageItem.mPhoneId);
@@ -563,7 +566,7 @@ public class MessageListItem extends ZoomMessageListItem implements
             }
         }
         drawRightStatusIndicator(mMessageItem);
-
+        mBodyTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mZoomFontSize);
         requestLayout();
     }
 
