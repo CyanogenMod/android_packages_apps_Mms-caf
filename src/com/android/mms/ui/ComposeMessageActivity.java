598592842b78922a8c546835bbc5157f2198a583
NBruderman <nbruderman@gmail.com>
2015-02-21 20:26:58 +0200

    MMS: Fix FC with the Unicode Stripping
    Seems to be an issue with String to int evaluation,
    so make everything a string and switch to equal strings method
    
    Change-Id: I3b54cf3a4ec65cfd45816d3a909aff051d7acd19

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2004,12 +2004,12 @@ public class ComposeMessageActivity extends Activity
         resetConfiguration(getResources().getConfiguration());
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        int unicodeStripping = prefs.getInt(MessagingPreferenceActivity.UNICODE_STRIPPING,
+        String unicodeStripping = prefs.getString(MessagingPreferenceActivity.UNICODE_STRIPPING,
                 MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT);
 
-        if (unicodeStripping != MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT) {
-            boolean stripNonDecodableOnly =
-                    unicodeStripping == MessagingPreferenceActivity.UNICODE_STRIPPING_NON_DECODABLE;
+        if (!(unicodeStripping.equals(MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT))) {
+            boolean stripNonDecodableOnly = unicodeStripping.equals
+                            (MessagingPreferenceActivity.UNICODE_STRIPPING_NON_DECODABLE);
             mUnicodeFilter = new UnicodeFilter(stripNonDecodableOnly);
         }
 
