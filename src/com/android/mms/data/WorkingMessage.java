d85aa72e1d17edfbb1210d17cc31f03f36c19c0f
kaiyiz <kaiyiz@codeaurora.org>
2015-03-20 14:52:39 +0800

    Mms: Display vCalendar attachment when viewing draft MMS
    After loading the draft MMS from provider, which one
    only contained calendar event attachment, Message
    application doesn't correct the attachment type to
    calendar event, it caused calendar event attachment lost.
    
    Correct the attachment type to calendar event, after
    loading  draft MMS which one only contained calendar
    event attachment.
    
    Change-Id: I51cff2765e518ee7a3084c4af97d620c7df46919
    CRs-Fixed: 808760

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -740,6 +740,8 @@ public class WorkingMessage {
                 mAttachmentType = AUDIO;
             } else if (slide.hasVcard()) {
                 mAttachmentType = VCARD;
+            } else if (slide.hasVCal()) {
+                mAttachmentType = VCAL;
             }
         }
 
