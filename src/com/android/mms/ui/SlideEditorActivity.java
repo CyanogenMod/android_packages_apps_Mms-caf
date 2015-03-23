0abe34e34d785408b6d0d9012d4031c13a9f736b
kaiyiz <kaiyiz@codeaurora.org>
2015-03-23 11:04:53 +0800

    Mms: Fix can not add maximum limit Record Audio after adding picture
    The parameter used for calculating the size limit is incorrect.
    
    Change the passed parameter value to zero.
    
    Change-Id: I7b5efdb9258e8b33a52ece34e3bc3cdf4aa85412
    CRs-Fixed: 810815

--- src/com/android/mms/ui/SlideEditorActivity.java
+++ src/com/android/mms/ui/SlideEditorActivity.java
@@ -520,11 +520,9 @@ public class SlideEditorActivity extends Activity {
                 break;
 
             case MENU_RECORD_SOUND:
-                slide = mSlideshowModel.get(mPosition);
-                int currentSlideSize = slide.getSlideSize();
-                long sizeLimit = ComposeMessageActivity.computeAttachmentSizeLimit(mSlideshowModel,
-                        currentSlideSize);
-                MessageUtils.recordSound(this, REQUEST_CODE_RECORD_SOUND, sizeLimit);
+                long soundSizeLimit = ComposeMessageActivity.computeAttachmentSizeLimit(
+                    mSlideshowModel, 0);
+                MessageUtils.recordSound(this, REQUEST_CODE_RECORD_SOUND, soundSizeLimit);
                 break;
 
             case MENU_DEL_AUDIO:
@@ -539,12 +537,10 @@ public class SlideEditorActivity extends Activity {
                 break;
 
             case MENU_TAKE_VIDEO:
-                slide = mSlideshowModel.get(mPosition);
-                currentSlideSize = slide.getSlideSize();
-                sizeLimit = ComposeMessageActivity.computeAttachmentSizeLimit(mSlideshowModel,
-                        currentSlideSize);
-                if (sizeLimit > 0) {
-                    MessageUtils.recordVideo(this, REQUEST_CODE_TAKE_VIDEO, sizeLimit);
+                long videoSizeLimit = ComposeMessageActivity.computeAttachmentSizeLimit(
+                    mSlideshowModel, 0);
+                if (videoSizeLimit > 0) {
+                    MessageUtils.recordVideo(this, REQUEST_CODE_TAKE_VIDEO, videoSizeLimit);
                 } else {
                     Toast.makeText(this,
                             getString(R.string.message_too_big_for_video),
