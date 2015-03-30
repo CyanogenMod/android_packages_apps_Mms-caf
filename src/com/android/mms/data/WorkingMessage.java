8334d601983767cfc8d5274a15271b1a8ee54ed3
xiao.hu <huxiao0816@gmail.com>
2015-03-30 06:03:55 -0700

    Mms-add Calendar event type of loading Calendar draft
    Repro:
    - Go to messaging-> add subject ->add an Calendar
    - back save as a draft->then type it->the Calendar cant see
    
    Change-Id: I1a54e5712a9ab4e16b9d3b793cff617e5d60218e

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -277,6 +277,8 @@ public class WorkingMessage {
                 mAttachmentType = AUDIO;
             } else if (slide.hasVcard()) {
                 mAttachmentType = VCARD;
+            } else if (slide.hasVCal()) {
+                mAttachmentType = VCAL;
             }
         }
 
