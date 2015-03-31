5fa42ae243b41bde03987051e660984b032e83ce
Danesh M <daneshm90@gmail.com>
2015-03-30 21:35:12 -0700

    Mms : Only exit after deleting last message
    Fixes bug where compose message (from action_view) and
    opening drafts auto-close
    
    Change-Id: I17c7a1f6062597f383fda5104de416de43d59639

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -5113,6 +5113,7 @@ public class ComposeMessageActivity extends Activity
                         }
                     }
 
+                    final int preCursorChangeCount = mMsgListAdapter.getCount();
                     mMsgListAdapter.changeCursor(cursor);
 
                     if (newSelectionPos != -1) {
@@ -5146,7 +5147,7 @@ public class ComposeMessageActivity extends Activity
                     // more people before the conversation begins.
                     if (cursor != null && cursor.getCount() == 0
                             && !isRecipientsEditorVisible() && !mSentMessage) {
-                        if (TextUtils.isEmpty(mTextEditor.getText())) {
+                        if (preCursorChangeCount >= 1 && TextUtils.isEmpty(mTextEditor.getText())) {
                             // No message was entered, dismiss
                             exitComposeMessageActivity(new Runnable() {
                                 @Override
