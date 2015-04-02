657409eab48bfe54063ef81487352afffeb41a7c
Joe Zhong <joe.zpf@gmail.com>
2015-04-02 11:24:15 +0800

    Mms: Recipients field is cleared when returning from SelectRecipients
    Repro:
     - Create a new conversation
     - Select some contacts
     - Pick contacts with "add contact" button
     - Swith to "Pick contacts" screen
     - Click home action bar in the top left corner.
     - Observe: in Compose screen, the previous information is gone
    
    Change-Id: I9c4765efecca86df7d2f233b6dc75b5a61d596c9

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -536,11 +536,15 @@ public class ComposeMessageActivity extends Activity
 
     private void pickContacts(int mode, int requestCode) {
         Intent intent = new Intent(ComposeMessageActivity.this, SelectRecipientsList.class);
-        if (mRecipientsEditor == null) {
-            initRecipientsEditor();
+        // avoid initializing mRecipientsEditor wrong. Otherwise, this will
+        // cause failure when saving a draft.
+        if(requestCode == REQUEST_CODE_ADD_RECIPIENTS) {
+            if (mRecipientsEditor == null) {
+                initRecipientsEditor();
+            }
+            ContactList contacts = mRecipientsEditor.constructContactsFromInput(false);
+            intent.putExtra(SelectRecipientsList.EXTRA_RECIPIENTS, contacts.getNumbers());
         }
-        ContactList contacts = mRecipientsEditor.constructContactsFromInput(false);
-        intent.putExtra(SelectRecipientsList.EXTRA_RECIPIENTS, contacts.getNumbers());
         intent.putExtra(SelectRecipientsList.MODE, mode);
         startActivityForResult(intent, requestCode);
     }
