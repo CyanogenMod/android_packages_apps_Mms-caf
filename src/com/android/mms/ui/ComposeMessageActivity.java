1490872e7e18be6afa131b7951da7b03334a12ce
Raj Yengisetty <rajesh@cyngn.com>
2015-04-06 16:15:58 -0700

    Mms: Fix for adding too many recipients to the Message
    There is a lot of jank when the user adds a large number of
    recipients to a message. This is because of the appends required
    to notify the RecipientsEditTextView of the new contacts that
    need to be "chipped." This is results in an unmanageable amount of
    work being done on the main thread. This workaround turns off
    chips when the user adds 15 contacts. RecipientsEditTextView already
    has an internal max of 50, but this is too high for this batched
    recipient entry.
    
    Change-Id: I0f3afe4fc686ba43dca35723532b81fd73bf007c

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -486,6 +486,8 @@ public class ComposeMessageActivity extends Activity
 
     private UnicodeFilter mUnicodeFilter = null;
 
+    private AddNumbersTask mAddNumbersTask;
+
     @SuppressWarnings("unused")
     public static void log(String logMsg) {
         Thread current = Thread.currentThread();
@@ -3637,7 +3639,8 @@ public class ComposeMessageActivity extends Activity
                 break;
 
             case REQUEST_CODE_ADD_RECIPIENTS:
-                insertNumbersIntoRecipientsEditor(
+                mAddNumbersTask = new AddNumbersTask();
+                mAddNumbersTask.execute(
                         data.getStringArrayListExtra(SelectRecipientsList.EXTRA_RECIPIENTS));
                 break;
 
@@ -3683,16 +3686,63 @@ public class ComposeMessageActivity extends Activity
         }
     };
 
-    private void insertNumbersIntoRecipientsEditor(final ArrayList<String> numbers) {
+    private void forwardRcsMessage(Intent data) {
+        ArrayList<String> numbers = data.getStringArrayListExtra(
+                SelectRecipientsList.EXTRA_RECIPIENTS);
         ContactList list = ContactList.getByNumbers(numbers, true);
-        ContactList existing = mRecipientsEditor.constructContactsFromInput(true);
-        for (Contact contact : existing) {
-            if (!contact.existsInDatabase()) {
-                list.add(contact);
+        long a = -1;
+        boolean success = false;
+        try {
+            ChatMessage message = mMessageApi.getMessageById(mRcsForwardId + "");
+            success = RcsChatMessageUtils.forwardMessage(a, Arrays.asList(list.getNumbers()),
+                    message);
+            if (success) {
+                toast(R.string.forward_message_success);
+            } else {
+                toast(R.string.forward_message_fail);
+            }
+        } catch (Exception e) {
+            toast(R.string.forward_message_fail);
+        }
+    }
+
+    class AddNumbersTask extends AsyncTask<ArrayList<String>, Void, Void> {
+        ProgressDialog mPD;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mPD = new ProgressDialog(ComposeMessageActivity.this);
+            mPD.setMessage("Adding contacts...");
+            mPD.show();
+        }
+
+        @Override
+        protected Void doInBackground(ArrayList<String>... params) {
+            if (params == null || params.length < 1) {
+                return null;
+            }
+
+            ArrayList<String> numbers = params[0];
+
+            ContactList list = ContactList.getByNumbers(numbers, true);
+            ContactList existing = mRecipientsEditor.constructContactsFromInput(true);
+            for (Contact contact : existing) {
+                if (!contact.existsInDatabase()) {
+                    list.add(contact);
+                }
+            }
+
+            mRecipientsEditor.populate(list);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (mPD != null && mPD.isShowing()) {
+                mPD.dismiss();
             }
         }
-        mRecipientsEditor.setText(null);
-        mRecipientsEditor.populate(list);
     }
 
     private void processPickResult(final Intent data) {
@@ -5296,7 +5346,7 @@ public class ComposeMessageActivity extends Activity
                 ComposeMessageActivity.this.mMsgListAdapter.notifyDataSetChanged();
 
                 if (mRecipientsEditor != null) {
-                    mRecipientsEditor.populate(recipients);
+                    // mRecipientsEditor.populate(recipients);
                 }
             }
         });
