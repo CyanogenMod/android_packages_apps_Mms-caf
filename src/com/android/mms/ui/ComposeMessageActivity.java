dbea1dcb42d2650380141f2dab318ffc4da68a86
d34d <clark@cyngn.com>
2015-06-09 14:50:38 -0700

    Don't filter share intent
    The share intent was being constructed such that the Mms app would
    not show up in the list.  It makes sense to include Mms because a
    user can share a message with a different person.
    
    The method used to filter out Mms also has an adverse side effect.
    Some of the options are displayed as "Android System" and there is
    no indication of what this action will do until the user clicks on
    one it.  This patch removes the filtering of Mms which also resolves
    the issue mentioned above, i.e. you will see "Copy to clipboard" as
    an option that was previously shown as "Android System"
    
    Change-Id: I217de2ff5ce00e05b72bf8403d15070d2ab85c9d
    (cherry picked from commit 7b40a127604731be1c35b3af1bcaac415e3cbeae)

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -5795,10 +5795,8 @@ public class ComposeMessageActivity extends Activity
             String body = c.getString(COLUMN_SMS_BODY);
             Intent shareIntent = getShareMessageIntent(body);
             Context ctx = getContext();
-            Intent chooserIntent =
-                    IntentUtils.createFilteredChooser(
-                            ctx, ctx.getString(R.string.message_share_intent_title),
-                            shareIntent, ctx.getPackageName());
+            Intent chooserIntent = Intent.createChooser(shareIntent,
+                    ctx.getString(R.string.message_share_intent_title));
             try {
                 startActivity(chooserIntent);
             } catch (ActivityNotFoundException e) {
