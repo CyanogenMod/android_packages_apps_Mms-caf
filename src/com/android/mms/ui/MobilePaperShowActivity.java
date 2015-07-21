dd82a7acdcb18934854d97b793bb0a0e5403faaa
Danesh M <daneshm90@gmail.com>
2015-07-21 10:26:40 -0700

    Mms : Ensure slideshow is not empty
    Change-Id: I95c2418900df4415539a113a5d04bf1107ad7bb7

--- src/com/android/mms/ui/MobilePaperShowActivity.java
+++ src/com/android/mms/ui/MobilePaperShowActivity.java
@@ -41,6 +41,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
@@ -185,15 +186,18 @@ public class MobilePaperShowActivity extends Activity {
                 setTitle("");
             }
 
-            String mailboxUri = MAILBOX_URI + mMailboxId;
-            mAsyncQueryHandler = new BackgroundQueryHandler(
-                    getContentResolver());
-            mAsyncQueryHandler.startQuery(QUERY_MESSAGE_TOKEN, 0,
-                    Uri.parse(mailboxUri),
-                    MessageListAdapter.MAILBOX_PROJECTION, "pdu._id= "
-                            + mUri.getLastPathSegment(), null,
-                    "normalized_date DESC");
-
+            if (mMailboxId != MailBoxMessageList.TYPE_DRAFTBOX) {
+                String mailboxUri = MAILBOX_URI + mMailboxId;
+                mAsyncQueryHandler = new BackgroundQueryHandler(
+                        getContentResolver());
+                mAsyncQueryHandler.startQuery(QUERY_MESSAGE_TOKEN, 0,
+                        Uri.parse(mailboxUri),
+                        MessageListAdapter.MAILBOX_PROJECTION, "pdu._id= "
+                                + mUri.getLastPathSegment(), null,
+                        "normalized_date DESC");
+            } else {
+                completeLoad();
+            }
         } catch (MmsException e) {
             Log.e(TAG, "Cannot present the slide show.", e);
             Toast.makeText(this, R.string.cannot_play, Toast.LENGTH_SHORT).show();
@@ -393,9 +397,13 @@ public class MobilePaperShowActivity extends Activity {
                 mCursor = cursor;
                 mCursor.moveToFirst();
             }
-            drawRootView();
-            invalidateOptionsMenu();
+            completeLoad();
         }
+    }
+
+    private void completeLoad() {
+        drawRootView();
+        invalidateOptionsMenu();
     }
 
     private void replyMessage(Context context, String number) {
