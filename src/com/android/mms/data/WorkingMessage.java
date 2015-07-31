4cc6d45f38ce5db0654b8a3b854777f5482c321c
Danesh M <daneshm90@gmail.com>
2015-07-31 13:24:57 -0700

    Mms : Replace  REQUEST_CODE_PICK with REQUEST_CODE_ADD_RECIPIENTS
    This ensures the logic surrounding the old REQUEST_CODE_PICK is still
    used. This fixes various draft issues when leaving the cma for picking
    recipients...etc
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java
    
    Change-Id: I52da4626415798f971a28933c4bbd4d1454eefd9

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -2477,6 +2477,14 @@ public class WorkingMessage {
         }, "WorkingMessage.asyncDelete").start();
     }
 
+    public void asyncDeleteDraftMessage(Conversation conv) {
+        if (mHasSmsDraft) {
+            asyncDeleteDraftSmsMessage(conv);
+        } else {
+            asyncDeleteDraftMmsMessage(conv);
+        }
+    }
+
     public void asyncDeleteDraftSmsMessage(Conversation conv) {
         mHasSmsDraft = false;
 
