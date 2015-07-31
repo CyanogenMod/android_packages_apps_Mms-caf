2d50e3dfd0829d3f203ef4ceab90098b87b28ef5
Danesh M <daneshm90@gmail.com>
2015-07-31 13:24:57 -0700

    Mms : Replace  REQUEST_CODE_PICK with REQUEST_CODE_ADD_RECIPIENTS
    This ensures the logic surrounding the old REQUEST_CODE_PICK is still
    used. This fixes various draft issues when leaving the cma for picking
    recipients...etc
    
    Change-Id: I52da4626415798f971a28933c4bbd4d1454eefd9

--- Android.mk
+++ Android.mk
@@ -37,12 +37,10 @@ LOCAL_AAPT_FLAGS := \
     --extra-packages com.android.phone.common
 
 LOCAL_REQUIRED_MODULES := SoundRecorder
-
+LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
 
-# This finds and builds the test apk as well, so a single make does both.
-include $(call all-makefiles-under,$(LOCAL_PATH))
