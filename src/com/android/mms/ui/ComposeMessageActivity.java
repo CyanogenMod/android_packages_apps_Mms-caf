40d7e09194b6568aa3d27f923d89eed9a4f8704e
Martin Brabham <optedoblivion@cyngn.com>
2015-07-31 11:33:34 -0700

    [MMS-53]: Upgrade recipient input (chips) for compose new message.
    Change-Id: I5c394e9eed2cd440cfe9aa6ae4f78c4e232dd799

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -76,6 +76,7 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SqliteWrapper;
 import android.drm.DrmStore;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
@@ -120,6 +121,7 @@ import android.util.TypedValue;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -1931,6 +1933,21 @@ public class ComposeMessageActivity extends Activity
         mRecipientsEditor.setAdapter(new ChipsRecipientAdapter(this));
         mRecipientsEditor.setText(null);
         mRecipientsEditor.populate(recipients);
+        float density = getResources().getDisplayMetrics().density;
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // Get display width
+        int width = size.x;
+
+        // Get the difference between display width and dropdown width
+        int offset = width - (303 * (int)density);
+        // Divide that by two so now we have the size of each side
+        offset /= 2;
+        // Make it negative to shift left
+        offset = -offset;
+        mRecipientsEditor.setDropDownHorizontalOffset(offset);
         mRecipientsEditor.setOnCreateContextMenuListener(mRecipientsMenuCreateListener);
         // TODO : Remove the max length limitation due to the multiple phone picker is added and the
         // user is able to select a large number of recipients from the Contacts. The coming
--- src/com/android/mms/ui/RecipientsEditor.java
+++ src/com/android/mms/ui/RecipientsEditor.java
@@ -40,12 +40,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.MultiAutoCompleteTextView;
 
+import com.android.contacts.common.lettertiles.LetterTileDrawable;
 import com.android.ex.chips.DropdownChipLayouter;
 import com.android.ex.chips.RecipientEditTextView;
-import com.android.ex.chips.RecipientEntry;
 import com.android.mms.MmsConfig;
 import com.android.mms.R;
 import com.android.mms.data.Contact;
@@ -76,7 +75,15 @@ public class RecipientsEditor extends RecipientEditTextView {
         }
     }
 
-    public RecipientsEditor(Context context, AttributeSet attrs) {
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int maxHeight = getResources().getDimensionPixelSize(R.dimen
+                .recipients_editor_maxHeight);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public RecipientsEditor(final Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mContext = context;
@@ -134,15 +141,30 @@ public class RecipientsEditor extends RecipientEditTextView {
             }
         });
 
-        setDropdownChipLayouter(new DropdownChipLayouter(LayoutInflater.from(context), context) {
+        setDropdownChipLayouter(new DropdownChipLayouter(LayoutInflater.from(context), context,
+                this) {
             @Override
             protected int getItemLayoutResId(AdapterType type) {
-                return R.layout.mms_chips_recipient_dropdown_item;
+                switch (type) {
+                    case BASE_RECIPIENT:
+                        return R.layout.mms_chips_recipient_dropdown_item;
+                    case RECIPIENT_ALTERNATES:
+                        return R.layout.mms_chips_recipient_dropdown_item_alternate;
+                    default:
+                        return super.getItemLayoutResId(type);
+                }
             }
 
             @Override
             protected int getAlternateItemLayoutResId(AdapterType type) {
-                return R.layout.mms_chips_recipient_dropdown_item;
+                switch (type) {
+                    case BASE_RECIPIENT:
+                        return R.layout.mms_chips_recipient_dropdown_item;
+                    case RECIPIENT_ALTERNATES:
+                        return R.layout.mms_chips_recipient_dropdown_item_alternate;
+                    default:
+                        return super.getItemLayoutResId(type);
+                }
             }
         });
     }
@@ -198,6 +220,19 @@ public class RecipientsEditor extends RecipientEditTextView {
             list.add(contact);
         }
         return list;
+    }
+
+    /**
+     * This blocks, only call from a background thread
+     *
+     * @param number {@link String}
+     * @return int
+     */
+    @Override
+    protected int getAccentColorForContact(String number) {
+        LetterTileDrawable lt = new LetterTileDrawable(getContext(), null);
+        lt.setContactDetails(null, number);
+        return lt.getColor();
     }
 
     private boolean isValidAddress(String number, boolean isMms) {
@@ -400,9 +435,15 @@ public class RecipientsEditor extends RecipientEditTextView {
         y += getScrollY();
 
         int line = layout.getLineForVertical(y);
-        int off = layout.getOffsetForHorizontal(line, x);
+        try {
+            int off = layout.getOffsetForHorizontal(line, x);
+            return off;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
-        return off;
+        return 0;
+
     }
 
     @Override
