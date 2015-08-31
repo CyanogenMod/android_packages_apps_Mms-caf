fdfe83d8843c713f79c665e9551f804b6ce20e92
Danesh M <daneshm90@gmail.com>
2015-08-30 20:24:50 -0700

    Mms : Fetch conversation info for search results
    Can't rely on database cursor to have this information, so instead
    fetch it asynchronously and populate the views.
    
    Change-Id: I4435b5580afdee2fb00cc2ac8c0c366916b9dbfc

--- src/com/android/mms/ui/SearchAdapter.java
+++ src/com/android/mms/ui/SearchAdapter.java
@@ -1,20 +1,25 @@
 package com.android.mms.ui;
 
-import com.android.mms.R;
-import com.android.mms.data.Contact;
-import com.android.mms.data.Conversation;
-
 import android.content.Context;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
-import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.android.mms.R;
+import com.android.mms.data.Contact;
+import com.android.mms.data.Conversation;
+
+import java.lang.ref.WeakReference;
 
 public class SearchAdapter extends CursorAdapter {
+
+    public static final int THREAD_ID_INDEX = 1;
+    private static final int BODY_INDEX = 3;
+    private static final int DATE_INDEX = 4;
+
     private String mQuery;
 
     public SearchAdapter(Context context) {
@@ -26,6 +31,7 @@ public class SearchAdapter extends CursorAdapter {
         private View attachment;
         private SearchActivity.TextViewSnippet body;
         private ImageView avatarView;
+        private FetchConversationTask task;
     }
 
     @Override
@@ -45,32 +51,58 @@ public class SearchAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ViewHolder holder = (ViewHolder) view.getTag();
-        Conversation conversation = Conversation.from(context, cursor);
-
-        holder.name.setText(conversation.getRecipients().formatNames(", "));
-        holder.date.setText(MessageUtils.formatTimeStampString(context, conversation.getDate()));
-        holder.attachment.setVisibility(conversation.hasAttachment() ? View.VISIBLE : View.GONE);
-        holder.body.setText(cursor.getString(cursor.getColumnIndex("body")), mQuery);
-
-        if (conversation.getRecipients().size() == 1) {
-            Contact contact = conversation.getRecipients().get(0);
-            contact.bindAvatar(holder.avatarView);
-        } else {
-            // TODO get a multiple recipients asset (or do something else)
-            holder.avatarView.setImageDrawable(ConversationListItem.sDefaultContactImage);
+        if (holder.task != null) {
+            holder.task.cancel(true);
         }
+        long threadId = cursor.getInt(THREAD_ID_INDEX);
+        holder.task = new FetchConversationTask(view, threadId);
+        holder.task.execute();
+
+        long date = cursor.getLong(DATE_INDEX);
+        holder.date.setText(MessageUtils.formatTimeStampString(context, date));
+        holder.body.setText(cursor.getString(BODY_INDEX), mQuery);
+        holder.avatarView.setImageDrawable(ConversationListItem.sDefaultContactImage);
     }
 
-    public Conversation getConversation(int pos) {
-        Cursor cursor = getCursor();
-        if (cursor == null) {
+    private static class FetchConversationTask extends AsyncTask<Void, Void, Conversation> {
+        private final WeakReference<View> mViewReference;
+        private final long mThreadId;
+
+        FetchConversationTask(View view, long threadId) {
+            mViewReference = new WeakReference<View>(view);
+            mThreadId = threadId;
+        }
+
+        @Override
+        protected Conversation doInBackground(Void... params) {
+            View view = mViewReference.get();
+            if (view != null) {
+                return Conversation.get(view.getContext(), mThreadId, true);
+            }
             return null;
         }
-        int originalPosition = cursor.getPosition();
-        Cursor cPosition = (Cursor) getItem(originalPosition);
-        Conversation conversation = Conversation.from(mContext, cPosition);
-        cursor.moveToPosition(originalPosition);
-        return conversation;
+
+        @Override
+        protected void onPostExecute(Conversation conversation) {
+            if (isCancelled()) {
+                return;
+            }
+            if (conversation != null) {
+                View view = mViewReference.get();
+                if (view != null) {
+                    ViewHolder holder = (ViewHolder) view.getTag();
+                    if (holder != null && holder.task == this) {
+                        holder.name.setText(conversation.getRecipients().formatNames(", "));
+                        holder.attachment.setVisibility(conversation.hasAttachment() ? View.VISIBLE : View.GONE);
+                        if (conversation.getRecipients().size() == 1) {
+                            Contact contact = conversation.getRecipients().get(0);
+                            contact.bindAvatar(holder.avatarView);
+                        }
+                    }
+                }
+
+            }
+        }
     }
 
     public void setQuery(String query) {
