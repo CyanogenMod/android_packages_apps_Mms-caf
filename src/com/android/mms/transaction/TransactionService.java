3a6cf193f4c111fa51970706057db441d8e850ca
Adnan Begovic <adnan@cyngn.com>
2015-09-11 10:23:58 -0700

    Mms: Utilize default sms sub id for mms transactions.
      A user may expect to have their MMS go over the
      default sms sub id even though an MMS is a data
      connection.
    
    Change-Id: I1bd03d74e7ad758a83b5690de5d7139d1fe1d06d
    TICKET: CYNGNOS-769
    (cherry picked from commit c468db0a6c6e76f8b6fa9e1fe5aa3537c13edb11)

--- src/com/android/mms/transaction/TransactionService.java
+++ src/com/android/mms/transaction/TransactionService.java
@@ -930,7 +930,7 @@ public class TransactionService extends Service implements Observer {
     }
 
     protected void endMmsConnectivity() {
-        int subId = SubscriptionManager.getOnDemandDataSubId();
+        int subId = getDefaultSmsSubId();
         endMmsConnectivity(subId);
     }
 
@@ -1034,7 +1034,7 @@ public class TransactionService extends Service implements Observer {
                     }
 
                     try {
-                        int subId = SubscriptionManager.getOnDemandDataSubId();
+                        int subId = getDefaultSmsSubId();
                         if (Log.isLoggable(LogTag.TRANSACTION, Log.VERBOSE)) {
                             Log.v(TAG, "renew PDP connection for subscription: " + subId);
                         }
@@ -1185,7 +1185,7 @@ public class TransactionService extends Service implements Observer {
 
                     if (!mPending.isEmpty()) {
                         try {
-                            beginMmsConnectivity(SubscriptionManager.getDefaultDataSubId());
+                            beginMmsConnectivity(getDefaultSmsSubId());
                         } catch (IOException e) {
                             Log.w(TAG, "Attempt to use of MMS connectivity failed");
                             return;
