b9e9ff200d81fb7c91581c8e9e5178ec8a57820c
Vineet Patil <vpatil@cyngn.com>
2015-10-29 16:38:54 -0700

    Fixed Messaging: Overflow menu options change after screen ON/OFF
    When screen is turned off and back on the contextual action mode
    options change even after the same item is selected.
    1. When we prepare the action Mode menu in onPrepareActionMode(),
    we did not update the mUnlockedCount and mMmsSelectedCount correctly.
    This change will update the statistics correctly.
    2. When an item is checked, in onItemCheckedStateChanged(),
    we did not update the counts statistics before setting up the
    visibilities of the action mode menu item. This change will update
    the statistics() before setting up the visibilities.
    
    Ticket: CYNGNOS-679
    
    Change-Id: Ic17c6922a515d28aa46f1262e93fb6b38c9e5799

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -5715,6 +5715,19 @@ public class ComposeMessageActivity extends Activity
                     item.setVisible(false);
                 }
             }
+
+            int checkedCount = getListView().getCheckedItemCount();
+            if (checkedCount > 0) { // action mode was restarted
+                updateCheckedStatus(mode, checkedCount);
+                // loop through checked items and update statistics
+                SparseBooleanArray checkedList = getListView().getCheckedItemPositions();
+                for (int i = 0; i < checkedList.size(); i++) {
+                    int position = checkedList.keyAt(i);
+                    boolean isChecked = checkedList.valueAt(i);
+                    updateStatics(position, isChecked);
+                    customMenuVisibility(mode, checkedCount, position, isChecked);
+                }
+            }
             return true;
         }
 
@@ -6129,7 +6142,7 @@ public class ComposeMessageActivity extends Activity
 
         @Override
         public void onItemCheckedStateChanged(ActionMode mode, int position,
-                long id, boolean checked) {
+                                              long id, boolean checked) {
             logMultiChoice("onItemCheckedStateChanged... position=" + position
                     + ", checked=" + checked);
 
@@ -6139,6 +6152,13 @@ public class ComposeMessageActivity extends Activity
             mode.setTitle(getString(R.string.selected_count, mCheckedCount));
 
             mode.getMenu().findItem(R.id.selection_toggle).setTitle(getString(
+                    allItemsSelected() ? R.string.deselected_all : R.string.selected_all));
+        }
+
+        private void updateCheckedStatus(ActionMode actionMode, int checkedCount) {
+            mCheckedCount = checkedCount;
+            actionMode.setTitle(getString(R.string.selected_count, mCheckedCount));
+            actionMode.getMenu().findItem(R.id.selection_toggle).setTitle(getString(
                     allItemsSelected() ? R.string.deselected_all : R.string.selected_all));
         }
 
